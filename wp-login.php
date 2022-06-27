db0018d1a50d6d49bab64d095c9aedce95c8a4d9
KingDoom <501210037@stu.itc.edu.vn>
2022-06-27 16:36:39 +0700

    v1

---
+++ wp-login.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Log In &lsaquo; Electro &#8212; WordPress</title>
+	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
+<link rel="dns-prefetch" href="//s.w.org">
+<script type="text/javascript">
+            window._nslDOMReady = function (callback) {
+                if ( document.readyState === "complete" || document.readyState === "interactive" ) {
+                    callback();
+                } else {
+                    document.addEventListener( "DOMContentLoaded", callback );
+                }
+            };
+            </script><link rel="stylesheet" id="fluentform-elementor-widget-css" href="https://phamhonghuy.github.io/Electro/wp-content/plugins/fluentform/public/css/fluent-forms-elementor-widget.css?ver=4.3.8" type="text/css" media="all">
+<link rel="stylesheet" id="dashicons-css" href="https://phamhonghuy.github.io/Electro/wp-includes/css/dashicons.min.css?ver=6.0" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://phamhonghuy.github.io/Electro/wp-includes/css/buttons.min.css?ver=6.0" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="https://phamhonghuy.github.io/Electro/wp-admin/css/forms.min.css?ver=6.0" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://phamhonghuy.github.io/Electro/wp-admin/css/l10n.min.css?ver=6.0" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="https://phamhonghuy.github.io/Electro/wp-admin/css/login.min.css?ver=6.0" type="text/css" media="all">
+	<meta name="referrer" content="strict-origin-when-cross-origin">
+		<meta name="viewport" content="width=device-width">
+		</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
+		<div id="login">
+		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="https://phamhonghuy.github.io/Electro/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" autocomplete="username">
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Password</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" autocomplete="current-password">
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Remember Me</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In">
+									<input type="hidden" name="redirect_to" value="https://phamhonghuy.github.io/Electro/wp-admin/">
+									<input type="hidden" name="testcookie" value="1">
+			</p>
+		</form>
+
+					<p id="nav">
+								<a href="https://phamhonghuy.github.io/Electro/my-account/lost-password/">Lost your password?</a>
+			</p>
+					<script type="text/javascript">
+			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
+				<p id="backtoblog">
+			<a href="https://phamhonghuy.github.io/Electro/">&larr; Go to Electro</a>		</p>
+			</div>
+				<div class="language-switcher">
+				<form id="language-switcher" action="" method="get">
+
+					<label for="language-switcher-locales">
+						<span class="dashicons dashicons-translation" aria-hidden="true"></span>
+						<span class="screen-reader-text">Language</span>
+					</label>
+
+					<br>
+<b>Warning</b>:  An unexpected error occurred. Something may be wrong with WordPress.org or this server&#8217;s configuration. If you continue to have problems, please try the <a href="https://wordpress.org/support/forums/">support forums</a>. (WordPress could not establish a secure connection to WordPress.org. Please contact your server administrator.) in <b>C:\xampp\htdocs\wp\wp-admin\includes\translation-install.php</b> on line <b>68</b><br>
+<select name="wp_lang" id="language-switcher-locales"><option value="en_US" lang="en" data-installed="1">English (United States)</option>
+<option value="vi" lang="" data-installed="1">vi</option></select>
+					
+											<input type="hidden" name="redirect_to" value="https://phamhonghuy.github.io/Electro/wp-admin/">
+					
+					
+						<input type="submit" class="button" value="Change">
+
+					</form>
+				</div>
+				<script type="text/javascript" src="https://phamhonghuy.github.io/Electro/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
+<script type="text/javascript" src="https://phamhonghuy.github.io/Electro/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+<script type="text/javascript" id="zxcvbn-async-js-extra">
+/* <![CDATA[ */
+var _zxcvbnSettings = {"src":"http:\/\/192.168.137.130\/wp\/electro\/wp-includes\/js\/zxcvbn.min.js"};
+/* ]]> */
+</script>
+<script type="text/javascript" src="https://phamhonghuy.github.io/Electro/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="https://phamhonghuy.github.io/Electro/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9" id="regenerator-runtime-js"></script>
+<script type="text/javascript" src="https://phamhonghuy.github.io/Electro/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script type="text/javascript" src="https://phamhonghuy.github.io/Electro/wp-includes/js/dist/hooks.min.js?ver=c6d64f2cb8f5c6bb49caca37f8828ce3" id="wp-hooks-js"></script>
+<script type="text/javascript" src="https://phamhonghuy.github.io/Electro/wp-includes/js/dist/i18n.min.js?ver=ebee46757c6a411e38fd079a7ac71d94" id="wp-i18n-js"></script>
+<script type="text/javascript" id="wp-i18n-js-after">
+wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
+</script>
+<script type="text/javascript" id="password-strength-meter-js-extra">
+/* <![CDATA[ */
+var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+/* ]]> */
+</script>
+<script type="text/javascript" src="https://phamhonghuy.github.io/Electro/wp-admin/js/password-strength-meter.min.js?ver=6.0" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="https://phamhonghuy.github.io/Electro/wp-includes/js/underscore.min.js?ver=1.13.3" id="underscore-js"></script>
+<script type="text/javascript" id="wp-util-js-extra">
+/* <![CDATA[ */
+var _wpUtilSettings = {"ajax":{"url":"\/wp\/electro\/wp-admin\/admin-ajax.php"}};
+/* ]]> */
+</script>
+<script type="text/javascript" src="https://phamhonghuy.github.io/Electro/wp-includes/js/wp-util.min.js?ver=6.0" id="wp-util-js"></script>
+<script type="text/javascript" id="user-profile-js-extra">
+/* <![CDATA[ */
+var userProfileL10n = {"user_id":"0","nonce":"d8f1535109"};
+/* ]]> */
+</script>
+<script type="text/javascript" src="https://phamhonghuy.github.io/Electro/wp-admin/js/user-profile.min.js?ver=6.0" id="user-profile-js"></script>
+	<div class="clear"></div>
+	</body>
+	</html>
